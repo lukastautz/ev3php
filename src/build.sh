e2c50c1da11aaeb5d735185339603dbb5c62ef1f
Lukas Tautz <104827162+lukastautz@users.noreply.github.com>
2022-11-05 13:20:54 +0100

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
-# ev3php module builder
+# ev3php module builder for crosscompiler
 # Copyright (C) 2022 Lukas Tautz
-# You need to install: build-essential and php7.0-dev
+# You need to install: arm-linux-gnueabi-gcc, arm-linux-gnueabi-strip and php7.0-dev
 echo "ev3php module builder"
 echo ""
 phpize
-./configure --enable-ev3php CFLAGS="-O9 -Ofast"
+./configure --enable-ev3php --host="arm-linux" CFLAGS="-O9 -Ofast -static" CC="arm-linux-gnueabi-gcc"
 make
+arm-linux-gnueabi-strip -R .comment modules/ev3php.so
+arm-linux-gnueabi-strip -R .note.gnu.build-id modules/ev3php.so
+arm-linux-gnueabi-strip -R .ARM.attributes modules/ev3php.so
+arm-linux-gnueabi-strip -R .gnu.version modules/ev3php.so
+arm-linux-gnueabi-strip --strip-unneeded modules/ev3php.so
 echo ""
 echo "Build finished"
 echo ""
